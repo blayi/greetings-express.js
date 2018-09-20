3dddb2b725e86a3a73af533a0c301304e56c6d29
blayi <siyasangablayi18@gmail.com>
2018-09-20 16:05:04 +0200

    added app json file

--- greeting.js
+++ greeting.js
@@ -22,7 +22,10 @@ module.exports = function GreetmeFunction(pool) {
         let userData = await pool.query('select * from users where name =$1', [enteredName])
        
         if (userData.rows.length === 0) {
-          await pool.query('insert into users (name, counter) values ($1, $2)', [enteredName, 1])
+          if(enteredName !=="" && selectedLang !== undefined){
+            await pool.query('insert into users (name, counter) values ($1, $2)', [enteredName, 1])
+
+          }
         } else {
           let increment = userData.rows[0].counter + 1;
           await pool.query('update users set counter=$1 where name =$2', [increment, enteredName])
@@ -40,8 +43,6 @@ module.exports = function GreetmeFunction(pool) {
 
   }
 
-
-
   var getNames = function () {
     return names;
   }
@@ -50,15 +51,22 @@ module.exports = function GreetmeFunction(pool) {
     let result = await pool.query('select count(*) from users ');
     return result.rows[0].count;
   }
-
-  var clear = function () {
-    names = {};
+var returnNames = async function(){
+let username = await pool.query('select * from users');
+return username.rows;
+}
+  var clear = async function () {
+    let reset = await pool.query('delete from users');
+  return reset ;
 
   }
 
   return {
     greet,
     getNames,
-    counter
+    counter,
+    returnNames,
+    clear
+
   }
 }
--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require('express');
 const exphbs  = require('express-handlebars');
 const bodyParser = require('body-parser');
 const GreetmeFunction =require('./greeting');
-
+const flash = require('express-flash');
+const session = require('express-session');
 const app = express();
 const pg = require("pg");
 const Pool = pg.Pool;
@@ -23,6 +24,15 @@ const pool = new Pool({
   });
 
 const greetFun = GreetmeFunction(pool);
+  // initialise session middleware - flash-express depends on it
+  app.use(session({
+    secret : "displaying error message",
+    resave: false,
+    saveUninitialized: true
+  }));
+
+  // initialise the flash middleware
+  app.use(flash());
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -35,18 +45,34 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-
 app.get('/',async function (req, res ) {
+  
   res.render('index');
+})
+app.post('/index',async function (req, res ) {
+  // let users= await greetFun.returnNames();
+  // let counter =await greetFun.counter();
+  res.render('index')})
+  
+app.get('/reset',async function (req, res ) {
+  let reset = await greetFun.clear();
+  res.render('greeted',{reset});
 })
 
 app.post('/greet', async function (req, res) {
   try{
     let enteredName = req.body.personName;
     let selectedLang = req.body.languageType;
+
+    if (enteredName == ""){
+      req.flash('info', 'enter name');
+    }
+    else if (selectedLang == undefined){
+      req.flash('info', 'select language');
+    }
     let greetPerson = await greetFun.greet(enteredName, selectedLang);
     let counter =await greetFun.counter();
-    // console.log(req.body);
+
       res.render('index',{
         greetPerson,
         counter
@@ -54,33 +80,21 @@ app.post('/greet', async function (req, res) {
   }
   catch(err){
     res.send(err.stack)
-  }
-  
-
+  }  
 });
 
+app.get("/greeted",async function(req, res){
+ let users= await greetFun.returnNames();
+ let counter =await greetFun.counter();
+ console.log(users);
+ 
+    res.render('greeted',{
+      users,
+      counter
+    })
+  
+})
 
-// app.get('/greet/:enteredName/:selectedLang', function(req, res) {
-//   let enteredName = req.params.enteredName;
-//   let selectedLang = req.params.selectedLang;
-//   let greetPerson = greetPeople.greet(selectedLang, enteredName);
-//   let greetCount = greetFun.counter();
-//
-//   res.render('greeting', {
-//     greetPerson,
-//     greetCount
-//   });
-// });
-
-// app.post('/greets', function (req, res) {
-//   let greetName = req.body.greeted_names;
-//   res.render('greets');
-// });
-//
-// app.post('/', function(req, res){
-//   greetPerson.reset();
-//
-// });
 
 let PORT = process.env.PORT || 3008;
 console.log(PORT)
